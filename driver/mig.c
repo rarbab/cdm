761f346d20e8edcd95d15c5d35479136d8fafeaa
Reza Arbab <arbab@linux.vnet.ibm.com>
2017-05-30 10:28:29 -0500

    Change {up,down}_write() to _read()

--- driver/mig.c
+++ driver/mig.c
@@ -93,7 +93,7 @@ int cdm_migrate(struct cdm_device *cdmdev, struct cdm_migrate *mig)
 	struct vm_area_struct *vma;
 	unsigned long addr, next;
 
-	down_write(&current->mm->mmap_sem);
+	down_read(&current->mm->mmap_sem);
 	vma = find_vma_intersection(current->mm, mig->start, mig->end);
 
 	for (addr = mig->start; addr < mig->end; addr = next) {
@@ -108,7 +108,7 @@ int cdm_migrate(struct cdm_device *cdmdev, struct cdm_migrate *mig)
 		if (rc)
 			return rc;
 	}
-	up_write(&current->mm->mmap_sem);
+	up_read(&current->mm->mmap_sem);
 
 	return 0;
 }
